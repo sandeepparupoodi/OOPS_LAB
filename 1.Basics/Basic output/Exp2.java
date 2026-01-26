a1923d0e9c25aad8649d340bec8fe502ef579e4c
sandeepparupoodi <venkatasandeepparupoodi@gmail.com>
2026-01-26 13:58:48 +0530

    Output

---
+++ 1.Basics/Basic output/Exp2.java
@@ -0,0 +1,11 @@
+class Exp2 
+{
+public static void main(String[] args) 
+{
+System.out.println("Welcome to OOP Lab!");
+int a = 10;
+int b = 15;
+int sum = a + b;
+System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+ }
+}
---
+++ 1.Basics/Basic output/Exp3.java
@@ -0,0 +1,13 @@
+public class Exp3 
+{
+public static void main(String[] args) 
+{
+System.out.println("This is about Java");
+// This is a single-line comment
+/*
+This is a multi-line comment.
+It can span multiple lines.
+*/
+}
+    
+}
